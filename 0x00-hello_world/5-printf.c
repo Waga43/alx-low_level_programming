731974c221277b24be55d6c7a4b6c4ed6b336fc9
waga43 <waga43tech@gmail,com>
2022-09-08 10:03:32 -0700

    Script uses 'printf' to print a sentence

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *
+ *This script uses the printf function
+ *
+ *Return - zero after execution
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
