8b586fdd25839f085fb309b76787f55ea367f069
waga43 <waga43tech@gmail,com>
2022-09-09 14:08:27 -0700

    Script prints all the numbers of base 16 in lowercase, followed by a new line.

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - function block
+ * Description- program prints all the numbers of base 16 in
+ * lowercase, followed by a new line.
+ *
+ * Return: zero returned
+ */
+int main(void)
+{
+	int x, y;
+	for (x = 0; x < 10; x++)
+	{
+		putchar (x + '0');
+	}
+	for (y = 'a'; y <= 'f'; y++)
+	{
+		putchar (y);
+	}
+
+putchar('\n');
+return (0);
+}
