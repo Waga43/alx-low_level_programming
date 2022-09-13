b8dde8026362acbb3436d7314989755927cd696b
waga43 <waga43tech@gmail,com>
2022-09-13 10:13:04 -0700

    Script checks for lowercase character.

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _islower(int c)- Function checks for lowercase character.
+ * Description- Checks for lowercase characters using the ASCII integer
+ * representation of lowercase alphabets a-z which is 97-122
+ *
+ * Returns: 1 if the character is lowercase and 0 if not lowercase.
+ **/
+
+int _islower(int c)
+{
+
+
+	return (c >= 97 && c <= 122);
+
+
+}
