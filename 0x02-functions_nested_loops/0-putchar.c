6ce16606e09fd0f6927cfd1f5f5f31f72223d434
waga43 <waga43tech@gmail,com>
2022-09-13 04:38:15 -0700

    Script prints _putchar on the screen

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - function block
+ * Description: Prints -putchar on the screen
+ * Return: zero is returned
+ */
+
+
+int main(){
+
+	printf("_putchar\n");
+	return 0;
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,3 @@
+# Introduction
+
+This directory contains codes for the ALX Software Engineering 0x02-functions_nested_loops projects.
