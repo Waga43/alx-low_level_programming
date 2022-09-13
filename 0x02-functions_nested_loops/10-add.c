f5d923b18357aeb82451eec82882464656a3d58d
waga43 <waga43tech@gmail,com>
2022-09-13 14:21:20 -0700

    Script adds two integers and returns the result.

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * add - Function checks for lowercase character.
+ * @num1: Function takes a first parameter num1 whose data type is int
+ * @num2: Second parameter, is also an integer
+ * Description: Function adds two integers and returns the result.
+ *
+ * Return: An integer result from adding two number integers
+ **/
+
+int add(int num1, int num2)
+
+{
+
+	int result;
+	result = num1 + num2;
+	return (result + '0');
+}
