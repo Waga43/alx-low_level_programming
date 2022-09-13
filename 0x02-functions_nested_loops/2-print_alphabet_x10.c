36d9e55e27030c03f3bc866b33bb9d7a360180a7
waga43 <waga43tech@gmail,com>
2022-09-13 09:38:59 -0700

    Script prints 10 times the alphabet, in lowercase, followed by a new line.

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * main - function block
+ * Description: Prints 10 times the alphabet, in lowercase, followed by a
+ * new line  on the screen using the _putchar function prototype declared
+ * in the main.h header file
+ *
+ * Return: zero is returned
+ */
+
+void print_alphabet_x10(void)
+{
+
+	int alphabet_1st;
+	int count = 0;
+
+	while (count < 10)
+	{
+		for (alphabet_1st >= 97; alphabet_1st <= 122; alphabet_1st++)
+		{
+
+			_putchar(alphabet_1st);
+		}
+
+
+	count++;
+	_putchar('\n');
+
+	}
+
+}
