f458366c871ceb50dc401790b415d5bcdbf8f029
waga43 <waga43tech@gmail,com>
2022-09-15 12:36:54 -0700

    Script finds prime numbers

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * main - Function finds and prints the largest prime factor
+ * of the number 612852475143 followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	long int num;
+	long int max;
+	long int x;
+
+	num = 612852475143;
+	max = -1;
+
+	while (num % 2 == 0)
+	{
+		max = 2;
+		num /= 2;
+	}
+
+	for (x = 3; x <= sqrt(num); x = x + 2)
+	{
+		while (num % x == 0)
+		{
+			max = x;
+			num = num / x;
+		}
+	}
+
+	if (num > 2)
+		max = num;
+
+	printf("%ld\num", max);
+
+	return (0);
+}
