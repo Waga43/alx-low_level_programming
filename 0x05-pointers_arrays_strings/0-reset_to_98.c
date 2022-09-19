114d324e21e1c79415bf39951819582d305e23e1
waga43 <waga43tech@gmail,com>
2022-09-19 13:18:36 -0700

    0-reset_to_98.c Script updates the value of a pointerto an int parameter

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *reset_to_98 - updates a integer value to 98
+ *@n: A pointer to an int parameter that will be updated
+ *
+ *Return: returns a void data type
+ */
+
+void reset_to_98(int *n)
+
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,3 @@
+# INTRODUCTION 
+
+This project contains scripts for the ALX SE 0x05. C - Pointers, arrays and strings0x05.
