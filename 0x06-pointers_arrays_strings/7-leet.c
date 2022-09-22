5c37579604a8e6034046d3c6134488a5e54f665d
waga43 <waga43tech@gmail,com>
2022-09-22 12:15:38 -0700

    7-leet.c edited

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -2,15 +2,14 @@
 
 /**
  * leet - Function encodes a string into 1337
- * @str: String characters to encode
- *
- * Description: Letters a and A should be replaced by 4,
+ * @str: Pointer String char parameter to encode
+ * Description: Letters a and A should be replaced by 4
  * Letters e and E should be replaced by 3
  * Letters o and O should be replaced by 0
  * Letters t and T should be replaced by 7
  * Letters l and L should be replaced by 1
  *
- * Return: newly encoded str characters
+ * Return: newly encoded str
  */
 
 char *leet(char *str)
@@ -20,16 +19,16 @@ char *leet(char *str)
 	char Str1[] = "AEOTL";
 	char str2[] = "43071";
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; str[i] != '\0'; i++)
 	{
 		for (ii = 0; ii < 5; ii++)
 		{
-			if (s[i] == s1[ii] || s[i] == S1[ii])
+			if (str[i] == str1[ii] || str[i] == Str1[ii])
 			{
-				s[i] = s2[ii];
+				str[i] = str2[ii];
 				break;
 			}
 		}
 	}
-	return (s);
+	return (str);
 }
