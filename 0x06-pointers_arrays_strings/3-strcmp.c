5a670f00d511d7754b4c8c2d3776ccd898191db5
waga43 <waga43tech@gmail,com>
2022-09-22 11:06:01 -0700

    Script edited

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -2,26 +2,21 @@
 /**
  * _strcmp - compares two strings
  * @s1: Frist string to be compared with s2
- * @s2: Second string to be compared
+ * @s2: Second string to be compared with s1
  *
  * Return: 0 if both strings match, otherwise, s1 - s2 if they don't match
 */
 
 int _strcmp(char *s1, char *s2)
 {
-int i;
+	int i;
 
-/**
- * We have reached the end of S1 OR we have reached the end of S2
- * 
-*/
-
-for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
 {
 	if (s1[i] != s2[i])
 	{
 		return (s1[i] - s2[i]);
 	}
 }
-return (0);
+	return (0);
 }
