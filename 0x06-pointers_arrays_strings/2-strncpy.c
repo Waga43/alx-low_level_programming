adf0301530741bc59eaeb6c29f7b7147f213b06e
waga43 <waga43tech@gmail,com>
2022-09-22 06:29:35 -0700

    Edited the 2-strncpy.c script

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,17 +9,24 @@
  * Return: A char string is returned
  */
 char *_strncpy(char *dest, char *src, int n)
+
 {
-	int index = 0, src_len = 0;
+	int i;
 
-	while (src[index++])
-		src_len++;
-
-	for (index = 0; src[index] && index < n; index++)
-		dest[index] = src[index];
-
-	for (index = src_len; index < n; index++)
-		dest[index] = '\0';
-
-	return (dest);
+	i = 0;
+	while (i < n && src[i] != '\0')
+{
+	dest[i] = src[i];
+	i++;
 }
+
+	while (i < n)
+{
+	dest[i] = '\0';
+	i++;
+}
+
+return (dest);
+
+}
+
