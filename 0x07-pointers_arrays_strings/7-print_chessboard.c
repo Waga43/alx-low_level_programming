8b9a807a371c7729388638ba49cbdbd8d8bb8568
waga43 <waga43tech@gmail,com>
2022-09-26 12:01:19 -0700

    7-print_chessboard.c- Function prints a cheesboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**i
+ * print_chessboard - Function that prints the chessboard.
+ * @a: parameter of char type
+ */
+void print_chessboard(char (*a)[8])
+{
+	
+	int x = 0;
+	int y = 0;
+
+	
+	while (x < 8)
+	{
+		while (y < 8)
+		{
+			_putchar (a[x][y]);
+			y++;
+		}
+		_putchar ('\n');
+		x++;
+	}
+
+}
