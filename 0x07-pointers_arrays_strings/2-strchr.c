48baecf1954319a7bfb6edc0cfc1e6a5fb3d49c4
waga43 <waga43tech@gmail,com>
2022-09-26 09:44:57 -0700

    2-strchr.c- Source code locates a character in a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _strchr - Locates a character in a string
+ * @s: String pointerparameter of char type
+ * @c: Character to search
+ *
+ * Return: Returns a pointer to the first occurrence of the character c 
+ * in the string s, or NULL if the character is not found
+ **/
+
+char *_strchr(char *s, char c)
+{
+
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+
+		++s;
+	}
+
+	if (*s == c)
+	{
+		return (s);
+
+	}
+
+
+	return (0);
+}
