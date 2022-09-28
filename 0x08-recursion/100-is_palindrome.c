5df1420dbe82ca0ec2c35398df71c6a6b5a3b092
waga43 <waga43tech@gmail,com>
2022-09-28 13:53:41 -0700

    100-is_palindrome.c Source code identifies if a string is a palindrome or not

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - Function returns the length of a string.
+ * @s: char string pointer parameter
+ * Return: int length of a string.
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	else
+		return (1 + _strlen_recursion(s + 1));
+}
+
+/**
+ * comparator - Function compares each character of the string.
+ * @s: char string pointer parameter
+ * @n1: int smallest iterator.
+ * @n2: int biggest iterator
+ *
+ * Return: an int
+ */
+int comparator(char *s, int n1, int n2)
+{
+	if (*(s + n1) == *(s + n2))
+	{
+		if (n1 == n2 || n1 == n2 + 1)
+			return (1);
+		return (0 + comparator(s, n1 + 1, n2 - 1));
+	}
+	return (0);
+}
+
+/**
+ * is_palindrome - detects if a string is a palindrome.
+ * @s: char string pointer parameter
+ * Return: 1 if s is a palindrome, and 0 if not.
+ */
+int is_palindrome(char *s)
+{
+	if (*s == '\0')
+		return (1);
+	return (comparator(s, 0, _strlen_recursion(s) - 1));
+}
