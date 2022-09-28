bc773f0e100d5b041fb9ebdbfd7dcee99a0c7982
waga43 <waga43tech@gmail,com>
2022-09-28 13:58:55 -0700

    101-wildcmp.c- Source code compares two strings to find out if they are identical

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * wildcmp - function compares two strings.
+ * @s1: char string pointer parameter
+ * @s2: char string pointer parameter
+ * Return: If the strings are identical, return 1 and return 0 if otherwise.
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
+		return (0);
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	if (*s2 == '*')
+		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
+	return (0);
+}
