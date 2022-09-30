383d609a500d49f1738967d54d023471181130aa
waga43 <waga43tech@gmail,com>
2022-09-30 14:40:54 -0700

    1-args.c- Source code prints the number of arguments passed into it

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Function prints the number of arguments passed into it.
+ * @argc: The count (number) of arguments used when calling the function
+ * @argv: char array string used when calling the function
+ *
+ * Return: zero 0 indicates successful program execution.
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
