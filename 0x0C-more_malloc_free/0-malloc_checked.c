e047cf291f906a0d93577e32f6656c599fd79355
waga43 <waga43tech@gmail,com>
2022-10-06 08:50:13 -0700

    Datatype edited

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -3,14 +3,14 @@
 /**
  * malloc_checked - Function allocates memory using malloc
  * @b: number of memory bytes to allocate
- * Description: f malloc fails, the malloc_checked function should cause
+ * Description: If malloc fails, the malloc_checked function should cause
  * normal process termination with a status value of 98
  *
  * Return: pointer to the allocated memory
  */
 void *malloc_checked(unsigned int b)
 {
-	void *ptr;
+	int *ptr;
 
 	ptr = malloc(b);
 	if (ptr == NULL)
