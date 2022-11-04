3c4699bc0d2f24b02648075f9f59053be69071ca
waga43 <waga43tech@gmail,com>
2022-11-04 09:01:47 -0700

    1-create_file.c- Program creates a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: the name of the file to create.
+ * @text_content:  is a NULL terminated string to write to the file
+ * Description: The created file must have those permissions: rw-------.
+ * If the file already exists, do not change the permissions.
+ * if the file already exists, truncate it
+ * if filename is NULL return -1
+ * if text_content is NULL create an empty file
+ *
+ * Return: 1 on success, -1 on failure (file can not be created, file can not
+ * be written, write “fails”, etc…)
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int nletters;
+	int rwr;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (fd == -1)
+		return (-1);
+
+	if (!text_content)
+		text_content = "";
+
+	for (nletters = 0; text_content[nletters]; nletters++)
+		;
+
+	rwr = write(fd, text_content, nletters);
+
+	if (rwr == -1)
+		return (-1);
+
+	close(fd);
+
+	return (1);
+}
