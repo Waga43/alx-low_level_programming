3e4023ec7bc44a15f034b36c036c71f0a5634a45
waga43 <waga43tech@gmail.com>
2024-03-22 08:20:18 -0700

    Edited lists.h file

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -3,7 +3,7 @@
 
 /*
  * File: lists.h
- * Auth: Bamidele Adefolaju
+ * Auth: Abdulwasiu Popoola
  */
 
 #include <stdio.h>
@@ -16,13 +16,13 @@
  * @next: points to the next node
  *
  * Description: doubly linked list node structure
- * for Holberton project
+ *
  */
 typedef struct dlistint_s
 {
-	int n;
-	struct dlistint_s *prev;
-	struct dlistint_s *next;
+    int n;
+    struct dlistint_s *prev;
+    struct dlistint_s *next;
 } dlistint_t;
 
 size_t print_dlistint(const dlistint_t *h);
